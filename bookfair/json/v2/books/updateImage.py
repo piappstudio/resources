fbc3de93a953317ada9f786f94967a41afa79ee3
Pi App Studio <70383910+piappstudio@users.noreply.github.com>
2023-01-09 19:51:45 -0600

    Updated with json

---
+++ bookfair/json/v2/books/updateImage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sys
+import json
+from json import dumps
+import requests
+from pathlib import Path
+
+
+from openpyxl import load_workbook
+json_file= sys.argv[1]
+folder = sys.argv[2]
+
+books = []
+with open(json_file, 'r') as f:
+  books = json.load(f)
+
+
+for book in books:
+  if book['images'] is not None:
+    updatedImages  = []
+    for img in book['images']:
+      res = img.split('/')
+      last = folder+res[len(res)-1]
+      updatedImages.append(last)
+      #Download image
+      img_data = requests.get(img).content
+      path = str(Path.cwd())+"/"+last
+      with open(path, 'wb') as handler:
+        handler.write(img_data)
+
+    book['images'] = updatedImages
+
+  print(books)
+  # convert into json
+json_data = dumps(books)
+print(json_data)
+
+
+out_file  = open (json_file, "w")
+out_file.write(json_data)
+out_file.close()
+
+
+
